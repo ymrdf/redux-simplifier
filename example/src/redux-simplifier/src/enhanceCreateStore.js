f2aecdd607146e87858ae479986c020c58911e84
ymrdf <837856276@qq.com>
2018-08-21 21:51:13 +0800

    delete some unnessary file

--- example/src/redux-simplifier/src/enhanceCreateStore.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux';
-import enhanceReducer from './enhanceReducer';
-
-//Enhance the reducers when creat a store;
-export default (reducer, preloadedState, enhancer) => createStore(enhanceReducer(reducer), preloadedState, enhancer);
